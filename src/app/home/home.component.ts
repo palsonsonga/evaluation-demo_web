c93fdb17a8964b3ea92015f5d32c497b0066eb43
palsonsonga <58547174+palsonsonga@users.noreply.github.com>
2021-01-25 03:58:08 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,19 @@
-﻿import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { first } from 'rxjs/operators';
-
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 import { User } from '@/_models';
 import { UserService, AuthenticationService } from '@/_services';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent implements OnInit {
-    currentUser: User;
-    users = [];
+    @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+    @ViewChild(MatSort, { static: true }) sort: MatSort;
 
+    currentUser: User;
+    users = new MatTableDataSource()
+    displayedColumns: string[] = ['name', 'role', 'action'];
     constructor(
         private authenticationService: AuthenticationService,
         private userService: UserService
@@ -29,6 +34,13 @@ export class HomeComponent implements OnInit {
     private loadAllUsers() {
         this.userService.getAll()
             .pipe(first())
-            .subscribe(users => this.users = users);
+            .subscribe(users => { 
+                this.users = new MatTableDataSource(users)
+                this.users.paginator=this.paginator
+                this.users.sort=this.sort
+            });
+    }
+    applyFilter(value) {
+        this.users.filter = value.trim().toLowerCase()
     }
 }
